1bc92ef76d96005d9cd49d2f2b80291f17e51f8b
Justin Liu <justin.liu6@student.tdsb.on.ca>
2024-12-10 11:30:33 -0500

    Finished Firebase setup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBBx-gFOlSdktcv3sJbmL6_yzkMtpK2Mug",
+    authDomain: "summitive-e4db9.firebaseapp.com",
+    projectId: "summitive-e4db9",
+    storageBucket: "summitive-e4db9.firebasestorage.app",
+    messagingSenderId: "1028730663711",
+    appId: "1:1028730663711:web:14dd00f4dd1bb3c285f62a"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
